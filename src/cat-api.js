da28a16109e2c59b2da9c8b08c3f6749a60f4c7d
yaromatv <122043732+yaromatv@users.noreply.github.com>
2023-06-22 20:11:12 +0200

    1 correction

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,35 +1,33 @@
-// const API_KEY =
-//   'live_wff01YUcmJjX9YEZvRl39ipXoQ88z558HPd3vtaNC3ighGssiNDpxK94FJVeGvpN';
+const API_KEY =
+  'live_wff01YUcmJjX9YEZvRl39ipXoQ88z558HPd3vtaNC3ighGssiNDpxK94FJVeGvpN';
 const BASE_URL = 'https://api.thecatapi.com/v1';
-let catDB = [];
-let catInfo = {};
 
 function fetchBreeds() {
   return fetch(`${BASE_URL}/breeds`).then(result =>
     result.json().then(data => {
-      catDB = data;
+      // console.log(data);
       return data;
     })
   );
 }
 
-function getBreedInfo(name) {
-  catDB.map(cat => {
-    if (name === cat.name) {
-      catInfo = {
-        name: cat.name,
-        desc: cat.description,
-        temper: cat.temperament,
-        id: cat.id,
-      };
-    }
-  });
-}
+function fetchCatByBreed(id) {
+  return fetch(
+    `${BASE_URL}/images/search?&api_key=${API_KEY}&breed_ids=${id}`
+  ).then(result =>
+    result.json().then(data => {
+      // GET PIC
+      const dataName = data[0].breeds[0];
 
-function fetchPic(id) {
-  return fetch(`${BASE_URL}/images/search?breed_ids=${id}`).then(result =>
-    result.json().then(data => data[0].url)
+      return (catInfo = {
+        pic: data[0].url,
+        name: dataName.name,
+        desc: dataName.description,
+        temper: dataName.temperament,
+        id: dataName.id,
+      });
+    })
   );
 }
 
-export { fetchBreeds, catInfo, fetchPic, getBreedInfo };
+export { fetchBreeds, fetchCatByBreed };
--- src/index.js
+++ src/index.js
@@ -1,49 +1,51 @@
-// 1 dostać liste gatunków kotików
-// 2 fn onSelect robi fetch info o kotiku
-
-// 3 zdenderować info o kotiku
 import { Report } from 'notiflix/build/notiflix-report-aio';
-import { fetchPic, fetchBreeds, catInfo, getBreedInfo } from '../src/cat-api';
+import { fetchCatByBreed, fetchBreeds } from '../src/cat-api';
 
 const selectEl = document.querySelector('.breed-select');
 const catInfoEl = document.querySelector('.cat-info');
 const loaderEl = document.querySelector('.loader');
 
+// MAIN
+renderBreeds();
+selectEl.addEventListener('change', onSelect);
+
 // RENDER
 function renderBreeds() {
-  // selectEl.classList.add('is-hidden');
   loaderEl.classList.remove('is-hidden');
 
   fetchBreeds()
     .then(data => {
+      // console.log(data);
       selectEl.classList.remove('is-hidden');
       loaderEl.classList.add('is-hidden');
 
-      data.map(cat => {
-        const markup = document.createElement('option');
-        markup.innerHTML = `<option value="${cat.id}">${cat.name}</option>`;
-        selectEl.append(markup);
-      });
+      const markup = data
+        .map(cat => {
+          return `<option value="${cat.id}">${cat.name}</option>`;
+        })
+        .join('');
+
+      selectEl.insertAdjacentHTML('afterbegin', markup);
+
+      // console.log(markup);
     })
     .catch(error => {
-      selectEl.classList.add('is-hidden');
+      loaderEl.classList.add('is-hidden');
+      // selectEl.classList.add('is-hidden');
       onError();
     });
 }
 
 function onSelect(e) {
-  getBreedInfo(e.target.value);
-
-  catInfoEl.classList.add('is-hidden');
   loaderEl.classList.remove('is-hidden');
 
-  fetchPic(catInfo.id)
+  fetchCatByBreed(e.target.value)
     .then(data => {
-      catInfoEl.classList.remove('is-hidden');
+      renderCat(data);
+
       loaderEl.classList.add('is-hidden');
-      return data;
+      catInfoEl.classList.remove('is-hidden');
     })
-    .then(data => renderCat(data))
     .catch(error => {
       onError();
       loaderEl.classList.add('is-hidden');
@@ -51,22 +53,11 @@ function onSelect(e) {
 }
 
 function onError() {
-  Report.failure('Kitties do not answer =(', '', 'Try again');
-  const reloadEl = document.querySelector('#NXReportButton');
-  reloadEl.addEventListener('click', () => {
-    window.location.reload();
-  });
+  Report.failure('Kitties do not answer =(', '', 'OK');
 }
 
-function renderCat(url) {
-  catInfoEl.innerHTML = `<div><img class="pic" src="${url}"/></div>
+function renderCat(catInfo) {
+  catInfoEl.innerHTML = `<div><img class="pic" src="${catInfo.pic}"/></div>
       <div class='cat-text'><h1>${catInfo.name}</h1><p class='cat-desc'>${catInfo.desc}</p><p><strong>Temperament: </strong>${catInfo.temper}</p></div>`;
-
-  const picEl = document.querySelector('.pic');
-  picEl.addEventListener('click', onSelect);
+  console.log(catInfoEl);
 }
-
-// MAIN
-renderBreeds();
-
-selectEl.addEventListener('change', onSelect);
